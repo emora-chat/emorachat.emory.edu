12e5cff1daa12b19fedd9f23bca886a1410c6071
Jinho Choi <jinho.choi@emory.edu>
2023-08-25 00:01:10 -0400

    Initial push

---
+++ footer.js
@@ -0,0 +1,144 @@
+document.write('\
+<footer>\
+    <div id="before-footer"></div>\
+    <div id="footer">\
+        <div class="site-footer ec-footer" id="siteFooter">\
+            <a class="sr-only sr-only-focusable sr-only-focusable--back" href="#main">Back to main content</a>\
+            <a class="sr-only sr-only-focusable sr-only-focusable--back" href="#top">Back to top</a>\
+            <div class="site-footer__ribbons" role="presentation">\
+                <div class="site-footer__ribbons-fold">\
+                    <span></span>\
+                </div>\
+                <div class="site-footer__ribbons-yellow"></div>\
+                <div class="site-footer__ribbons-blue"></div>\
+            </div>\
+            <a class="emory-logo" href="http://www.emory.edu">\
+                <img alt="Emory University home" class="site-footer__eu-logo-footer" src="https://secure.web.emory.edu/webcollege/template/st4.1/images/eu-logo-footer.svg"/>\
+            </a>\
+            <div class="site-footer__content">\
+                <div class="container">\
+                    <nav aria-label="Primary footer navigation" class="site-footer__btn-nav">\
+                        <a class="nav-link" href="http://college.emory.edu/oue/academic-calendar.html" target="_blank">\
+                            <span aria-hidden="true" class="fa-fw fas fa-calendar mr-1"></span>\
+                            <span>Academic Calendar</span>\
+                        </a>\
+                        <a class="nav-link" href="http://atlas.college.emory.edu" target="_blank">\
+                            <span aria-hidden="true" class="fa-fw fas fa-atlas mr-1"></span>\
+                            <span>Course Atlas</span>\
+                        </a>\
+                        <a class="nav-link" href="http://catalog.college.emory.edu" target="_blank">\
+                            <span aria-hidden="true" class="fa-fw fas fa-book mr-1"></span>\
+                            <span>College Catalog</span>\
+                        </a>\
+                        <a class="nav-link" href="http://college.emory.edu/oue" target="_blank">\
+                            <span aria-hidden="true" class="fa-fw fas fa-share-alt mr-1"></span>\
+                            <span>Undergrad Office</span>\
+                        </a>\
+                    </nav>\
+                    <div class="row flex-sm-row-reverse">\
+                        <div class="col-lg-6 mb-4">\
+                            <a class="custom-logo ec-logo" href="http://college.emory.edu" target="_blank">\
+                                <img alt="Emory College of Arts &amp; Sciences Logo" class="img-fluid mb-3" src="https://secure.web.emory.edu/webcollege/template/st4.1/images/ec-logo-hz-white.svg"/>\
+                            </a>\
+                            <div class="mb-2 text-lg-right">\
+                                <a aria-label="facebook" class="site-footer__social-button" href="https://www.facebook.com/emorycollege/" target="_blank">\
+                                    <span aria-hidden="true" class="fab fa-facebook"></span>\
+                                </a>\
+                                <a aria-label="twitter" class="site-footer__social-button" href="https://twitter.com/EmoryCollege" target="_blank">\
+                                    <span aria-hidden="true" class="fab fa-twitter"></span>\
+                                </a>\
+                                <a aria-label="instagram" class="site-footer__social-button" href="https://www.instagram.com/EmoryCollege/" target="_blank">\
+                                    <span aria-hidden="true" class="fab fa-instagram"></span>\
+                                </a>\
+                                <a aria-label="snapchat" class="site-footer__social-button" href="https://www.snapchat.com/add/EmoryCollege/index.html" target="_blank">\
+                                    <span aria-hidden="true" class="fab fa-snapchat"></span>\
+                                </a>\
+                            </div>\
+                            <div class="wysiwyg text-lg-right">\
+                                <ul class="list-unstyled">\
+                                    <li>\
+                                        <a href="http://college.emory.edu/main/administration/index.html" rel="noopener" target="_blank">\
+                                            <strong>Emory College Offices</strong>\
+                                        </a>\
+                                    </li>\
+                                    <li>\
+                                        <a href="http://college.emory.edu/main/administration/leadership/index.html" rel="noopener" target="_blank">\
+                                            <strong>Emory College Leadership</strong>\
+                                        </a>\
+                                    </li>\
+                                </ul>\
+                            </div>\
+                        </div>\
+                        <div class="col-lg-6 mb-4">\
+                            <address>\
+                                <div class="wysiwyg">\
+                                    <h5 class="text-light mt-0">Office of Communications</h5>\
+                                    <p>\
+                                        <a href="https://goo.gl/maps/SAcQGGw9efJ1vtEH6" target="_blank">\
+                                            Emory College of Arts &amp;Sciences <br/>\
+                                            1627 North Decatur Road <br/>Atlanta, Georgia 30322\
+                                        </a>\
+                                    </p>\
+                                    <div class="mb-3">\
+                                        <div class="small">\
+                                            <a href="mailto:ecas.comm@emory.edu">\
+                                                <span aria-hidden="true" class="fa-fw fas fa-envelope mr-2"></span>\
+                                                <span>ecas.comm@emory.edu</span>\
+                                            </a>\
+                                        </div>\
+                                        <div class="small">\
+                                            <a href="tel:404-727-6069">\
+                                                <span aria-hidden="true" class="fa-fw fas fa-phone mr-2"></span>\
+                                                <span>404-727-6069</span>\
+                                            </a>\
+                                        </div>\
+                                        <div class="small">\
+                                            <a href="tel:404-727-1069">\
+                                                <span aria-hidden="true" class="fa-fw fas fa-fax mr-2"></span>\
+                                                <span>404-727-1069</span>\
+                                            </a>\
+                                        </div>\
+                                    </div>\
+                                </div>\
+                            </address>\
+                            <a aria-label="facebook" class="site-footer__social-button" href="https://www.facebook.com/emorycollege/" target="_blank">\
+                                <span aria-hidden="true" class="fab fa-facebook"></span>\
+                            </a>\
+                            <a aria-label="twitter" class="site-footer__social-button" href="https://twitter.com/EmoryCollege" target="_blank">\
+                                <span aria-hidden="true" class="fab fa-twitter"></span>\
+                            </a>\
+                            <a aria-label="instagram" class="site-footer__social-button" href="https://www.instagram.com/EmoryCollege/" target="_blank">\
+                                <span aria-hidden="true" class="fab fa-instagram"></span>\
+                            </a>\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>\
+            <div class="site-post-footer" id="postFooter">\
+                <div class="container py-2 text-center">\
+                    <div class="row">\
+                        <div class="col-sm-4">\
+                            <small>\
+                                Copyright © <a href="http://www.emory.edu/" target="_blank">Emory University</a>\
+                            </small>\
+                        </div>\
+                        <div class="col-sm-4">\
+                            <small>\
+                                <a href="http://equityandinclusion.emory.edu/about/resources/eoaa.html" target="_blank">EO/AA Statement</a>\
+                                | <a href="http://communications.emory.edu//resources/copyright.html" target="_blank">Copyright</a>\
+                            </small>\
+                        </div>\
+                        <div class="col-sm-4">\
+                            <a class="font-weight-bold" href="#">\
+                                <span>Back to top</span>\
+                                <span aria-hidden="true" class="fas fa-angle-double-up ml-2"></span>\
+                            </a>\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>\
+    </div>\
+    <div id="after-footer"></div>\
+</footer>\
+');
---
+++ header.js
@@ -0,0 +1,118 @@
+document.write('\
+<header>\
+    <div id="before-header">\
+        <noscript>\
+            <p class="bg-warning text-center mb-0 p-1">Please enable Javascript in your web browser in order to use the features on this website.</p>\
+        </noscript>\
+        <div class="skip-top sr-only" id="top">Top of page</div>\
+        <a class="sr-only sr-only-focusable sr-only-focusable--skip" href="#main">Skip to main content</a>\
+    </div>\
+    <div id="header">\
+        <div class="header standard-header fixed-top" id="siteHeader">\
+            <nav aria-label="Main site navigation" class="navbar navbar-expand-lg">\
+                <a class="header-logo" href="index.html">\
+                    <img alt="Standard Template 4.1 Preview home" class="header-logo__svg" src="https://secure.web.emory.edu/webcollege/template/st4.1/images/eu-logo-shield.svg"/>\
+                </a>\
+                <div class="header-sitename">\
+                    <img alt="Emory College of Arts and Sciences" class="header-sitename__svg header-ecas__svg" src="https://secure.web.emory.edu/webcollege/template/st4.1/images/ec-logo-text-hz.svg"/>\
+                    <a class="header-sitename__title" href="index.html">Emora Chat</a>\
+                </div>\
+                <button aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler collapsed" data-target="#mainNav" data-toggle="offcanvas" type="button">\
+                    <span>Menu</span>\
+                </button>\
+                <div class="collapse navbar-collapse-standard navbar-collapse-offcanvas" id="mainNav">\
+                    <div class="header-menus">\
+                        <div class="header-menus__top">\
+                            <div class="header-nav-aux">\
+                                <ul class="navbar-nav flex-lg-wrap justify-content-start justify-content-lg-end" id="nav-aux">\
+                                    <li class="nav-item">\
+                                    </li>\
+                                </ul>\
+                            </div>\
+                        </div>\
+                        <div class="header-menus__bottom">\
+                            <div class="header-nav-main">\
+                                <ul aria-labelledby="nav-menu-home-toggle" class="navbar-nav nav-level-1" id="nav-main">\
+                                    <li class="nav-item dropdown">\
+                                        <a aria-expanded="false" aria-haspopup="true" class="dropdown-toggle nav-link" data-toggle="menu" href="#" id="nav-menu-about-toggle" role="button">\
+                                            <span>About</span>\
+                                        </a>\
+                                        <ul aria-labelledby="nav-menu-about-toggle" class="dropdown-menu nav-level-2" id="nav-menu-about">\
+                                            <li>\
+                                                <a class="dropdown-item" href="about.html" target="_self">\
+                                                    <span>Emora Chat</span>\
+                                                </a>\
+                                            </li>\
+                                            <li>\
+                                                <a class="dropdown-item" href="contact.html" target="_self">\
+                                                    <span>Contact</span>\
+                                                </a>\
+                                            </li>\
+                                        </ul>\
+                                    </li>\
+                                    <li class="nav-item dropdown">\
+                                        <a aria-expanded="false" aria-haspopup="true" class="dropdown-toggle nav-link" data-toggle="menu" href="#" id="nav-menu-chatbots-toggle" role="button">\
+                                            <span>Chatbots</span>\
+                                        </a>\
+                                        <ul aria-labelledby="nav-menu-chatbots-toggle" class="dropdown-menu nav-level-2" id="nav-menu-chatbots">\
+                                            <li>\
+                                                <a class="dropdown-item" href="getting_started.html" target="_self">\
+                                                    <span>Getting Started</span>\
+                                                </a>\
+                                            </li>\
+                                            <li>\
+                                                <a class="dropdown-item" href="release_notes.html" target="_self">\
+                                                    <span>Release Notes</span>\
+                                                </a>\
+                                            </li>\
+                                            <div class="dropdown-divider"></div>\
+                                            <li>\
+                                                <a class="dropdown-item" href="information_bot.html" target="_self">\
+                                                    <span>Information Bot</span>\
+                                                </a>\
+                                            </li>\
+                                            <li>\
+                                                <a class="dropdown-item" href="assistant_bot.html" target="_self">\
+                                                    <span>Assistant Bot</span>\
+                                                </a>\
+                                            </li>\
+                                            <li>\
+                                                <a class="dropdown-item" href="companion_bot.html" target="_self">\
+                                                    <span>Companion Bot</span>\
+                                                </a>\
+                                            </li>\
+                                        </ul>\
+                                    </li>\
+                                    <li class="nav-item">\
+                                        <a class="nav-link" href="faq.html" target="_self">\
+                                            <span>FAQ</span>\
+                                        </a>\
+                                    </li>\
+                                </ul>\
+                            </div>\
+                            <form action="https://search.emory.edu/" class="form-inline header-search m-0" method="GET" target="_blank">\
+                                <input name="output" type="hidden" value="xml_no_dtd"/>\
+                                <input name="filter" type="hidden" value="0"/>\
+                                <input name="proxystylesheet" type="hidden" value="homepage"/>\
+                                <div class="input-group">\
+                                    <!-- Search dropdown-toggle -->\
+                                    <div class="input-group-append">\
+                                        <button aria-label="Toggle search" class="btn btn-emory-blue header-search__toggle d-none d-lg-flex" id="search-toggle" title="Toggle search" type="button"></button>\
+                                    </div>\
+                                    <!-- Search input box -->\
+                                    <input aria-label="Enter keywords" class="form-control header-search__input" id="search-input" name="q" placeholder="Enter keywords..." type="text"/>\
+                                    <!-- Search submit -->\
+                                    <div class="input-group-append">\
+                                        <input aria-label="Submit search query" class="btn btn-light-blue header-search__submit" id="search-submit" name="search" type="submit" value="Search"/>\
+                                    </div>\
+                                </div>\
+                            </form>\
+                        </div>\
+                    </div>\
+                </div>\
+            </nav>\
+        </div>\
+    </div>\
+    <div id="after-header"></div>\
+</header>\
+');
